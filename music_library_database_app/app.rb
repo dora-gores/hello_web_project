17353a4efe35662a3d42a1a2f158f1005e5b3298
Dora Gores <d.lopesgores@gmail.com>
2023-02-07 19:11:54 +0000

    rearanged files

--- music_library_database_app/app.rb
+++ music_library_database_app/app.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 require "sinatra/reloader"
 require_relative 'lib/database_connection'
 require_relative 'lib/album_repository'
-# require_relative 'lib/artist_repository'
+require_relative 'lib/artist_repository'
 
 DatabaseConnection.connect
 
@@ -12,50 +12,45 @@ class Application < Sinatra::Base
     register Sinatra::Reloader
     # also_reload 'lib/album_repository'
     # also_reload 'lib/artist_repository'
-    get '/albums' do
-      repo = AlbumRepository.new
-      albums = repo.all
+  end
+  
+  get '/albums' do
+    repo = AlbumRepository.new
+    albums = repo.all
+    response = albums.map do |album|
+      album.title
+    end.join(', ')
+    
+    return response
+  end
 
-      response = albums.map do |album|
-        album.title
-      end.join(', ')
-      
-      return response
-    end
+  post '/albums' do
+    repo = AlbumRepository.new
+    new_album = Album.new
+    new_album.title = params[:title]
+    new_album.release_year = params[:release_year]
+    new_album.artist_id = params[:artist_id]
+    repo.create(new_album)
+    return ''
+  end
 
-    post '/albums' do
-      repo = AlbumRepository.new
-      new_album = Album.new
-      new_album.title = params[:title]
-      new_album.release_year = params[:release_year]
-      new_album.artist_id = params[:artist_id]
+  post '/artists' do
+    repo = ArtistRepository.new
+    new_artist = Artist.new
+    new_artist.name = params[:name]
+    new_artist.genre = params[:genre]
+    repo.create(new_artist)
+    return ''
+  end
 
-      repo.create(new_album)
-
-      return ''
-
-    end
-
-    post '/artists' do
-      repo = ArtistRepository.new
-      new_artist = Artist.new
-      new_artist.name = params[:name]
-      new_artist.genre = params[:genre]
-
-      repo.create(new_artist)
-
-      return ''
-    end
-
-    get '/artists' do
-      repo = ArtistRepository.new
-      artists = repo.all
-
-      response = artists.map do |artist|
-        artist.name
-      end.join(', ')
-      
-      return response
-    end
+  get '/artists' do
+    repo = ArtistRepository.new
+    artists = repo.all
+    response = artists.map do |artist|
+      artist.name
+    end.join(', ')
+    
+    return response
+  
   end
 end
