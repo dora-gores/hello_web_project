558afff22123937e91f102ffb43dd7012102b26a
Dora Gores <d.lopesgores@gmail.com>
2023-02-07 14:02:15 +0000

    Finished GET /albums

---
+++ music_library_database_app/spec/integration/application_spec.rb
@@ -0,0 +1,20 @@
+require "spec_helper"
+require "rack/test"
+require_relative '../../app'
+
+describe Application do
+  include Rack::Test::Methods
+
+  let(:app) { Application.new }
+
+  context "GET /albums" do
+    it 'returns a list of albums' do
+      # Assuming the post with id 1 exists.
+      response = get('/albums')
+      expected_response = 'Surfer Rosa, Waterloo, Super Trouper, Bossanova, Lover, Folklore, I Put a Spell on You, Baltimore, Here Comes the Sun, Fodder on My Wings, Ring Ring'
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq(expected_response)
+    end
+  end
+end
