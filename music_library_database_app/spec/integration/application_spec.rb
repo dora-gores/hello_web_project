4e738b817d24ffd7728ffc26f24eeb0b1f5b4fea
Dora Gores <d.lopesgores@gmail.com>
2023-02-08 11:43:32 +0000

    Removed extra GET /artists test

--- music_library_database_app/spec/integration/application_spec.rb
+++ music_library_database_app/spec/integration/application_spec.rb
@@ -2,7 +2,19 @@ require "spec_helper"
 require "rack/test"
 require_relative '../../app'
 
+
+def reset_artists_table
+  seed_sql = File.read('spec/seeds/artists_seeds.sql')
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'music_library_test' })
+  connection.exec(seed_sql)
+end
+
 describe Application do
+
+  before(:each) do
+    reset_artists_table
+  end
+
   include Rack::Test::Methods
 
   let(:app) { Application.new }
@@ -49,17 +61,17 @@ describe Application do
 
       response = get('/artists')
 
-      expect(response.body).to include('Billie Eilish')
+      expect(response.body).to eq('Pixies, ABBA, Taylor Swift, Nina Simone, Billie Eilish')
     end
   end
 
-  context "GET /artists" do
-    it 'returns a list of artists' do
-      response = get('/artists')
-      expected_response = 'Pixies, ABBA, Taylor Swift, Nina Simone, Kiasmos, Billie Eilish'
+  # context "GET /artists" do
+  #   it 'returns a list of artists' do
+  #     response = get('/artists')
+  #     expected_response = 'Pixies, ABBA, Taylor Swift, Nina Simone, Billie Eilish'
 
-      expect(response.status).to eq(200)
-      expect(response.body).to eq(expected_response)
-    end
-  end
+  #     expect(response.status).to eq(200)
+  #     expect(response.body).to eq(expected_response)
+  #   end
+  # end
 end
